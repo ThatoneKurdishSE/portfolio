6f0198e64686b67265f88b1c4b5f03faf3a51860
Raz <razaljaf@gmail.com>
2023-09-02 12:02:18 -0400

    cleaned up a bit

--- src/atoms/Box/Card/BlogCards.js
+++ src/atoms/Box/Card/BlogCards.js
@@ -4,13 +4,12 @@ import { blogCard } from "./blogCard.scss"
 const BlogCards = () => {
 
     return (
-            <div style={{overflowX: "auto"}}>
-                <table style={{display: "flex", flexWrap: "wrap", justifyContent: "space-evenly"}}>
+            <div style={{display: "flex", flexWrap: "wrap", justifyContent: "space-evenly"}}>
                 <div class="card">
-                        <span class="tag tag-brown">Medium article</span>
-                    <img src="https://miro.medium.com/v2/resize:fit:640/format:webp/1*szlsZC24vK9iTv6iin1_eg.jpeg" class="card__image" alt="brown couch" />
+                        <span class="tag tag-medium">Medium article</span>
+                    <img src="https://miro.medium.com/v2/resize:fit:640/format:webp/1*szlsZC24vK9iTv6iin1_eg.jpeg" class="card__image" alt="medium" />
                     <div class="card__content">
-                        <time datetime="2023-07-07" class="card__date">7 July 2023</time>
+                        <time dateTime="2023-07-07" class="card__date">7 July 2023</time>
                         <span class="card__title">
                             <a class="card__title" href="https://medium.com/@razaljaf/traveling-with-my-spices-and-learning-from-turtles-through-our-shared-home-of-planet-earth-594307172eae">
                                 Traveling with my spices and learning from turtles through our shared home of planet earth.
@@ -19,10 +18,10 @@ const BlogCards = () => {
                     </div>
                 </div>
                 <div class="card">
-                        <span class="tag tag-brown">Medium article</span>
-                    <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*KfOa-WLu4sB6mHNha52IFQ.png" class="card__image" alt="brown couch" />
+                        <span class="tag tag-medium">Medium article</span>
+                    <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*KfOa-WLu4sB6mHNha52IFQ.png" class="card__image" alt="medium" />
                     <div class="card__content">
-                        <time datetime="2023-06-02" class="card__date">2 June 2023</time>
+                        <time dateTime="2023-06-02" class="card__date">2 June 2023</time>
                         <span class="card__title">
                             <a class="card__title" href="https://medium.com/@razaljaf/traveling-with-my-spices-and-learning-from-turtles-through-our-shared-home-of-planet-earth-594307172eae">
                             Laid off, the capitalistic hook?
@@ -31,10 +30,10 @@ const BlogCards = () => {
                     </div>
                 </div>
                 <div class="card">
-                        <span class="tag tag-brown">Medium article</span>
-                    <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*RL2AMMGaPI98up7-aG4dsA.jpeg" class="card__image" alt="brown couch" />
+                        <span class="tag tag-medium">Medium article</span>
+                    <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*RL2AMMGaPI98up7-aG4dsA.jpeg" class="card__image" alt="medium" />
                     <div class="card__content">
-                        <time datetime="2023-02-15" class="card__date">15 February 2023</time>
+                        <time dateTime="2023-02-15" class="card__date">15 February 2023</time>
                         <span class="card__title">
                             <a class="card__title" href="https://medium.com/design-bootcamp/using-interior-design-principles-to-apply-dark-mode-to-your-application-b92ba96f1e66">
                             Using interior design principles to apply dark mode to your application.
@@ -43,10 +42,10 @@ const BlogCards = () => {
                     </div>
                 </div>
                 <div class="card">
-                <span class="tag tag-brown">Medium article</span>
-                    <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*qeCjWQptNaOyoX8anhez5A.jpeg" class="card__image" alt="brown couch" />
+                <span class="tag tag-medium">Medium article</span>
+                    <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*qeCjWQptNaOyoX8anhez5A.jpeg" class="card__image" alt="medium" />
                     <div class="card__content">
-                        <time datetime="2023-11-05" class="card__date">5 November 2022</time>
+                        <time dateTime="2023-11-05" class="card__date">5 November 2022</time>
                         <span class="card__title">
                             <a class="card__title" href="https://medium.com/@razaljaf/pema-my-non-profit-dream-98e56c832515">
                             Pema, my non-profit dream.
@@ -55,10 +54,10 @@ const BlogCards = () => {
                     </div>
                 </div>
                 <div class="card">
-                <span class="tag tag-brown">Medium article</span>
-                    <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*mVaNocGVxxv9MPq-N1kCOA.jpeg" class="card__image" alt="brown couch" />
+                <span class="tag tag-medium">Medium article</span>
+                    <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*mVaNocGVxxv9MPq-N1kCOA.jpeg" class="card__image" alt="medium" />
                     <div class="card__content">
-                        <time datetime="2021-05-06" class="card__date">6 May 2021</time>
+                        <time dateTime="2021-05-06" class="card__date">6 May 2021</time>
                         <span class="card__title">
                             <a class="card__title" href="https://medium.com/@razaljaf/7-junior-developer-job-interviews-in-3-days-the-what-i-learned-edition-74b005bdea14">
                                 7 junior developer job interviews in 3 days. (The what I learned edition).
@@ -67,10 +66,10 @@ const BlogCards = () => {
                     </div>
                 </div>
                 <div class="card">
-                <span class="tag tag-brown">Medium article</span>
-                    <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*85MCi-aJp6UpMSNlGwvVHQ.jpeg" class="card__image" alt="brown couch" />
+                <span class="tag tag-medium">Medium article</span>
+                    <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*85MCi-aJp6UpMSNlGwvVHQ.jpeg" class="card__image" alt="medium" />
                     <div class="card__content">
-                        <time datetime="2021-03-23" class="card__date">23 March 2021</time>
+                        <time dateTime="2021-03-23" class="card__date">23 March 2021</time>
                         <span class="card__title">
                             <a class="card__title" href="https://medium.com/@razaljaf/surviving-a-coding-bootcamp-mindfulness-tools-edition-15ecd3ce110e">
                                 Surviving a coding bootcamp: Mindfulness tools edition.
@@ -79,10 +78,10 @@ const BlogCards = () => {
                     </div>
                 </div>
                 <div class="card">
-                <span class="tag tag-brown">Medium article</span>
-                    <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*XCI9bLo1EjEbVWG6ZaEycw.jpeg" class="card__image" alt="brown couch" />
+                <span class="tag tag-medium">Medium article</span>
+                    <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*XCI9bLo1EjEbVWG6ZaEycw.jpeg" class="card__image" alt="" />
                     <div class="card__content">
-                        <time datetime="2021-03-14" class="card__date">14 March 2021</time>
+                        <time dateTime="2021-03-14" class="card__date">14 March 2021</time>
                         <span class="card__title">
                             <a class="card__title" href="https://medium.com/@razaljaf/go-fiber-the-skinny-28432421a696">
                                 Go Fiber(the skinny)
@@ -90,7 +89,6 @@ const BlogCards = () => {
                         </span>
                     </div>
                 </div>
-            </table>
         </div>
     );
 
